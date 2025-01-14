00715bd0ccc1e45ac6f2db0eae2ca2fe1201a0a0
ArkarZinNaing <arkarzinnaing38@gmail.com>
2025-01-14 10:43:53 +0700

    remove preload company

--- src/app/pages/exhibitor-registration/exhibitor-registration.component.ts
+++ src/app/pages/exhibitor-registration/exhibitor-registration.component.ts
@@ -53,7 +53,6 @@ export class ExhibitorRegistrationComponent implements OnInit {
 
 
   ngOnInit() {
-    this.loadCompanies();
     this.loadCountries();
     this.addExhibitor(); // Add first exhibitor card by default
   }
